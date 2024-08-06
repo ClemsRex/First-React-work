b006373fbd59c200ea1c394470a7fd9cf0d848bc
ClemsRex <clementchukwu351@gmail.com>
2024-08-06 19:50:27 +0100

    First React push

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+import Navbar from './Component/Navbar'
+import Signin from './Component/Signin'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Signin/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Component/Signin.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Signin = () => {
+  return (
+    <div>
+      <h2>Sign up with us Here</h2>
+    </div>
+  )
+}
+
+export default Signin
